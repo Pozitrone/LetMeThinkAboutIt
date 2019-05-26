cfc8dd7395612c9ac86474c4e1afce755b3f6fff
Pozitrone <jirirkas.skola@gmail.com>
2019-05-26 08:56:55 +0200

    Created basic NeuralNetwork library

---
+++ nnLib.js
@@ -0,0 +1,7 @@
+const NeuralNetwork = (InputAmount, HiddenAmount, OutputAmount) => {
+    this.input_nodes = InputAmount;
+    this.hidden_nodes = HiddenAmount;
+    this.output_nodes = OutputAmount;
+
+
+}
