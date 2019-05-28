6dee58eae3d7b8d52c98636cdd3b8df9bc6a7b8c
Pozitrone <jirirkas.skola@gmail.com>
2019-05-28 21:53:34 +0200

    Added static functions, added map and localEdit, added setup

--- matrixLib.js
+++ matrixLib.js
@@ -2,78 +2,134 @@ class Matrix {
     constructor (rows, columns) {
         this.rows = rows;
         this.columns = columns;
-        this.matrix = Array();
+        this.data = Array();
 
         for (let i = 0; i < this.rows; i++){
-            this.matrix[i] = Array();
+            this.data[i] = Array();
             for (let j = 0; j < this.columns; j++){
-                this.matrix[i][j] = 0;
+                this.data[i][j] = 0;
             }
         }
     }
 
-    add(n) {
+    add(n){
         if(n instanceof Matrix){
             for (let i = 0; i < this.rows; i++){
                 for (let j = 0; j < this.columns; j++){
-                    this.matrix[i][j] *= n.matrix[i][j];
+                    this.data[i][j] *= n.data[i][j];
                 }
             }
         }
         else{
             for (let i = 0; i < this.rows; i++){
                 for (let j = 0; j < this.columns; j++){
-                    this.matrix[i][j] *= n;
+                    this.data[i][j] *= n;
                 }
             }
         }  
     }
 
-    multiply(n) {
-        if(n instanceof Matrix){
-            for (let i = 0; i < this.rows; i++){
-                for (let j = 0; j < this.columns; j++){
-                    this.matrix[i][j] += n.matrix[i][j];
-                }
-            }
+    static multiply(m ,n){
+        if(m.columns !== n.rows){
+            throw ("Columns of A != Rows of B");
         }
         else{
-            for (let i = 0; i < this.rows; i++){
-                for (let j = 0; j < this.columns; j++){
-                    this.matrix[i][j] += n;
+            let a = m.rows;
+            let b = m.columns;
+            let c = n.columns;
+
+            var C = new Matrix(a,c);
+            for (let i = 0; i < a; i++){
+                for (let j = 0; j < c; j++){
+                    let sum = 0;
+                    for (let k = 0; k<b; k++){
+                        sum += m.data[i][k] * n.data[k][j];
+                    }
+                    C.data[i][j] = sum;
                 }
             }
-        }  
+            return C;
+        }
     }
 
-    randomize() {
+    multiply(n){
         for (let i = 0; i < this.rows; i++){
             for (let j = 0; j < this.columns; j++){
-                this.matrix[i][j] = Math.floor(Math.random() * 10);
+                this.data[i][j] *= n;
+            }
+        }
+         
+    }
+
+    transpose(){
+        let m = new Matrix(this.columns,this.rows);
+
+        for (let i = 0; i < this.rows; i++){
+            for (let j = 0; j < this.columns; j++){
+                m.data[j][i] = this.data[i][j];
+            }
+        }
+        this.data = m.data;
+        this.columns = m.columns;
+        this.rows = m.rows;
+    }
+
+    static transpose(){
+        let m = new Matrix(this.columns,this.rows);
+
+        for (let i = 0; i < this.rows; i++){
+            for (let j = 0; j < this.columns; j++){
+                m.data[j][i] = this.data[i][j];
+            }
+        }
+        return m;
+    }
+
+    map(fn){
+        // Apply function to every element
+        for (let i = 0; i < this.rows; i++){
+            for (let j = 0; j < this.columns; j++){
+                let val = this.data[i][j];
+                this.data[i][j] = fn(val);
             }
         }
     }
 
-    mult(mx) {
-        let n = this.rows;
-        let m = this.columns;
-        let p = mx.columns;
-
-        var C = new Matrix(n,p);
-        for (let i = 0; i < n; i++){
-            for (let j = 0; j < p; j++){
-                let sum = 0;
-                for (let k = 0; k<m; k++){
-                    sum += this.matrix[i][k] * mx.matrix[k][j];
-                }
-                C.matrix[i][j] = sum;
-            }
+    localEdit(fn, i, j){
+        if(i >= 0 && j >= 0){
+            let val = this.data[i][j];   
+            this.data[i][j] = fn(val);
         }
-        return C;
+        else{
+            throw("Incorrect index.");
+        }
     }
 
-    log() {
-        console.table(this.matrix);
+
+
+    randomize(){
+        for (let i = 0; i < this.rows; i++){
+            for (let j = 0; j < this.columns; j++){
+                this.data[i][j] = Math.floor(Math.random() * 10);
+            }
+        } 
     }
+
+    log(){
+        console.table(this.data);
+    }
+}
+
+function setup(){
+    let m = new Matrix(2,3);
+    let n = new Matrix(3,2);
+    m.randomize();
+    n.randomize();
+    m.log();
+    n.log();
+
+
+    var c = m.multiply(n);
+    c.log();
 }
 
